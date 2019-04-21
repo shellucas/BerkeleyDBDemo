1206c7d6ac9acb9705ae591eb055b9768d1c2a42
KamiKoroshi <lucasvdlaan@gmail.com>
2019-04-21 15:54:38 +0200

    JAR fix

--- src/com/shellucas/DatabaseHandler.java
+++ src/com/shellucas/DatabaseHandler.java
@@ -5,6 +5,9 @@ import com.sleepycat.je.rep.CommitPointConsistencyPolicy;
 import com.sleepycat.je.rep.utilint.RepUtils;
 
 import java.io.File;
+import java.net.URISyntaxException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.Objects;
 import java.util.UUID;
 import java.util.concurrent.TimeUnit;
@@ -22,7 +25,14 @@ class DatabaseHandler {
   
   static Environment createENV(String dbLocation) {
     Environment env;
-    final File jarFile = new File(Objects.requireNonNull(ClassLoader.getSystemClassLoader().getResource(".")).getPath());
+    Path path = null;
+    try {
+      path = Paths.get(Main.class.getProtectionDomain().getCodeSource().getLocation().toURI()).getParent();
+    } catch (URISyntaxException e) {
+      e.printStackTrace();
+    }
+    assert path != null;
+    final File jarFile = path.toFile();
     final File envDir = new File(jarFile.getPath() + dbLocation);
     boolean dirCreated = envDir.mkdirs();
     
--- src/com/shellucas/commands/CommandFactory.java
+++ src/com/shellucas/commands/CommandFactory.java
@@ -3,6 +3,7 @@ package com.shellucas.commands;
 public class CommandFactory {
     public static Command getCommand(int cmd) {
         switch (cmd) {
+            case 0: System.exit(0);
             case 1: return new CreateCommand();
             case 2: return new GetCommand();
             case 3: return new UpdateCommand();
@@ -12,12 +13,11 @@ public class CommandFactory {
     }
     
     public static String getMenu() {
-        StringBuilder builder = new StringBuilder();
-        builder.append("Commands:\n");
-        builder.append("1: Create\n");
-        builder.append("2: Read\n");
-        builder.append("3: Update\n");
-        builder.append("4: Delete\n");
-        return builder.toString();
+        return "Commands:\n" +
+                "0: Exit\n" +
+                "1: Create\n" +
+                "2: Read\n" +
+                "3: Update\n" +
+                "4: Delete";
     }
 }
