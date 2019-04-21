631f980ff635b6e58eb4abc9e9eaf9825e7df5e4
KamiKoroshi <lucasvdlaan@gmail.com>
2019-04-21 15:33:40 +0200

    Update works

--- src/com/shellucas/commands/DeleteCommand.java
+++ src/com/shellucas/commands/DeleteCommand.java
@@ -4,10 +4,23 @@ import com.shellucas.PersonHandler;
 import com.sleepycat.je.Database;
 import com.sleepycat.je.Environment;
 
+import java.util.Random;
+import java.util.Scanner;
+
 public class DeleteCommand implements Command {
+    private Scanner scanner = new Scanner(System.in);
+    
     @Override
     public int execute(PersonHandler ph, Environment env, Database db) {
-
-        return -1;
+        System.out.print(" Enter key: ");
+        int key = scanner.nextInt();
+        
+        boolean deleted = ph.delete(key, env, db);
+        
+        if (deleted) {
+            System.out.println("Entry has been deleted");
+        }
+        
+        return 0;
     }
 }
